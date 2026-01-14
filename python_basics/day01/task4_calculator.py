0641221f3078baab21a9e673acdfff96445e1e7e
quotex220420-crypto <quotex220420@gmail.com>
2026-01-14 16:03:20 +0530

    Added all Python Day 1 assignment programs

---
+++ python_basics/day01/task4_calculator.py
@@ -0,0 +1,8 @@
+num1 = 10
+num2 = 5
+
+print("First Number:", num1)
+print("Second Number:", num2)
+print("Sum:", num1 + num2)
+print("Difference:", num1 - num2)
+print("Multiplication:", num1 * num2)
