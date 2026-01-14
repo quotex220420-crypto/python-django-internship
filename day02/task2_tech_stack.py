231fad9c3d04e4789b72962d9e607d81d8ef007c
quotex220420-crypto <quotex220420@gmail.com>
2026-01-14 22:36:29 +0530

    Added Python Day 2 assignment programs

---
+++ day02/task2_tech_stack.py
@@ -0,0 +1,8 @@
+front_end = ["HTML", "CSS", "JavaScript", "React"]
+back_end = ["Python", "Django", "Node.js", "MySQL"]
+
+print("Front-end Technologies:")
+print(front_end)
+
+print("\nBack-end Technologies:")
+print(back_end)
