e9e15f4dcac18b4cc38be9fe8be67d943b434a6c
quotex220420-crypto <quotex220420@gmail.com>
2026-01-14 22:41:48 +0530

    Delete day02 directory

--- day02/task3_python_working.py
+++
@@ -1,9 +0,0 @@
-print("What is a Python Interpreter?")
-print("A Python interpreter reads Python code line by line and executes it.")
-
-print("\nHow Python code runs step by step:")
-print("1. You write Python code in a file.")
-print("2. The interpreter checks the syntax.")
-print("3. It converts the code into bytecode.")
-print("4. The bytecode is executed line by line.")
-print("5. The result is shown as output.")
